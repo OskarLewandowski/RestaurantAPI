5cf112b165de0d48026948bebf5a5af873c5ff47
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-26 14:36:11 +0200

    clean code, delete weatherforecast files

--- RestaurantAPI/Controllers/IWeatherForecastService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace RestaurantAPI.Controllers
-{
-    public interface IWeatherForecastService
-    {
-        IEnumerable<WeatherForecast> Get(int take, int minTemperature, int maxTemperature);
-    }
-}
