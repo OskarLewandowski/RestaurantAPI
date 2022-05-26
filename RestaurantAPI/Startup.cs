d2bfc50a53bb643186dfdaae9cfba353d70694ca
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-26 14:25:51 +0200

    add seed data class

--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RestaurantAPI.Controllers;
+using RestaurantAPI.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,12 +34,17 @@ namespace RestaurantAPI
                 {
                     options.JsonSerializerOptions.WriteIndented = true;
                 });
+            services.AddDbContext<RestaurantDbContext>();
+            services.AddScoped<RestaurantSedder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, RestaurantSedder sedder)
         {
             //Middlewares
+
+            sedder.Seed();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
