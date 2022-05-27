361f2e065321277ffd6ea97e11a4f12f1109793b
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-27 15:45:15 +0200

    add new entity user, role

--- RestaurantAPI/Entities/RestaurantDbContext.cs
+++ RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -16,7 +16,8 @@ namespace RestaurantAPI.Entities
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Dish> Dishes { get; set; }
-
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -38,8 +39,15 @@ namespace RestaurantAPI.Entities
                 .Property(e => e.Street)
                 .IsRequired()
                 .HasMaxLength(50);
-        }
 
+            modelBuilder.Entity<User>()
+                .Property(e => e.Email)
+                .IsRequired();
+
+            modelBuilder.Entity<Role>()
+                .Property(e => e.Name)
+                .IsRequired();
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
