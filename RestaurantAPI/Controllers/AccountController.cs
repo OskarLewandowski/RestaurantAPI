b2bd71b4a0bbd7b140ee9cef2810a431c1f104d2
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-27 16:29:28 +0200

    add classes and endpoint to register new user

---
+++ RestaurantAPI/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Models;
+using RestaurantAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("register")]
+        public ActionResult RegisterUser([FromBody] RegisterUserDto userDto)
+        {
+            _accountService.RegisterUser(userDto);
+
+            return Ok();
+        }
+    }
+}
---
+++ RestaurantAPI/Services/AccountService.cs
@@ -0,0 +1,40 @@
+﻿using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Services
+{
+    public interface IAccountService
+    {
+        void RegisterUser(RegisterUserDto userDto);
+    }
+
+    public class AccountService : IAccountService
+    {
+        private readonly RestaurantDbContext _context;
+
+        public AccountService(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        public void RegisterUser(RegisterUserDto userDto)
+        {
+            var newUser = new User()
+            {
+                Email = userDto.Email,
+                PasswordHash = userDto.Password,
+                DateOfBirth = userDto.DateOfBirth,
+                Nationality = userDto.Nationality,
+                RoleId = userDto.RoleId,
+            };
+
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+        }
+    }
+}
