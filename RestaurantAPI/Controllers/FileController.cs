29a1c393f232a9502316d866163f9f1e3d6289cf
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-29 18:56:48 +0200

    add endpoint to send file to the server

--- RestaurantAPI/Controllers/FileController.cs
+++ RestaurantAPI/Controllers/FileController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 using System;
@@ -33,6 +34,25 @@ namespace RestaurantAPI.Controllers
             var fileContents = System.IO.File.ReadAllBytes(filePath);
 
             return File(fileContents, contentType, fileName);
+        }
+
+        [HttpPost]
+        public ActionResult Upload([FromForm] IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                var rootPath = Directory.GetCurrentDirectory();
+                var fullPath = $"{rootPath}/PrivateFiles/PostedFiles/{file.FileName}";
+
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                return Ok();
+            }
+
+            return BadRequest();
         }
     }
 }
