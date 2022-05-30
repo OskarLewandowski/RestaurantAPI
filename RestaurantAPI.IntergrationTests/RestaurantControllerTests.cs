6aa2109fb873730f1c60e95432d2a4039fb456ce
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-30 17:47:39 +0200

    add first integration test

--- RestaurantAPI.IntergrationTests/RestaurantControllerTests.cs
+++ RestaurantAPI.IntergrationTests/RestaurantControllerTests.cs
@@ -8,14 +8,47 @@ namespace RestaurantAPI.IntergrationTests
 {
     public class RestaurantControllerTests
     {
-        [Fact]
-        public void GetAll_WithQueryParameters_ReturnsOkResult()
+        [Theory]
+        [InlineData(1, 5)]
+        [InlineData(1, 10)]
+        [InlineData(1, 15)]
+        public async void GetAll_WithQueryParameters_ReturnsOkResult(int pageNumber, int pageSize)
         {
             //arrange
 
+            var factory = new WebApplicationFactory<Program>();
+            var client = factory.CreateClient();
+
             //act
 
+            var response = await client.GetAsync($"/api/restaurant?pageNumber={pageNumber}&pageSize={pageSize}");
+
             //assert
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+
+        [Theory]
+        [InlineData(1, 55)]
+        [InlineData(11, 30)]
+        [InlineData(1, 155)]
+        [InlineData(null, 155)]
+        [InlineData(0, 0)]
+        public async void GetAll_WithInvalidQueryParameters_ReturnsBadRequest(int pageNumber, int pageSize)
+        {
+            //arrange
+
+            var factory = new WebApplicationFactory<Program>();
+            var client = factory.CreateClient();
+
+            //act
+
+            var response = await client.GetAsync($"/api/restaurant?pageNumber={pageNumber}&pageSize={pageSize}");
+
+            //assert
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
         }
     }
 }
--- RestaurantAPI.IntergrationTests/Usings.cs
+++ RestaurantAPI.IntergrationTests/Usings.cs
@@ -1,3 +1,4 @@
 global using Xunit;
 global using Microsoft.AspNetCore.Mvc.Testing;
 global using Microsoft.VisualStudio.TestPlatform.TestHost;
+global using FluentAssertions;
