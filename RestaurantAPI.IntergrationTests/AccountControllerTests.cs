a31b1dde78b150a483af294ecf8fd8b762ef8726
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-31 19:44:37 +0200

    added test to check endpoint Login, using mock service, MOQ

--- RestaurantAPI.IntergrationTests/AccountControllerTests.cs
+++ RestaurantAPI.IntergrationTests/AccountControllerTests.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Authorization.Policy;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using RestaurantAPI.Entities;
 using RestaurantAPI.IntergrationTests.Helpers;
 using RestaurantAPI.Models;
+using RestaurantAPI.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +21,7 @@ namespace RestaurantAPI.IntergrationTests
     public class AccountControllerTests : IClassFixture<WebApplicationFactory<Startup>>
     {
         private readonly HttpClient _client;
+        private readonly Mock<IAccountService> _accountServiceMock = new Mock<IAccountService>();
 
         public AccountControllerTests(WebApplicationFactory<Startup> factory)
         {
@@ -33,11 +36,39 @@ namespace RestaurantAPI.IntergrationTests
 
                         service.Remove(dbContextOptions);
 
+                        service.AddSingleton<IAccountService>(_accountServiceMock.Object);
+
                         service.AddDbContext<RestaurantDbContext>(options =>
                             options.UseInMemoryDatabase("RestaurantDbInMemory"));
                     });
                 })
                 .CreateClient();
+        }
+
+        [Fact]
+        public async Task Login_ForRegisteredUser_ReturnOk()
+        {
+            //arrange
+
+            _accountServiceMock
+                .Setup(e => e.GenerateJwt(It.IsAny<LoginDto>()))
+                .Returns("jwt");
+
+            var loginDto = new LoginDto()
+            {
+                Email = "test@test.com",
+                Password = "password123"
+            };
+
+            var httpContent = loginDto.ToJsonHttpContent();
+
+            //act
+
+            var response = await _client.PostAsync("/api/account/login", httpContent);
+
+            //asserts
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
         }
 
         [Fact]
