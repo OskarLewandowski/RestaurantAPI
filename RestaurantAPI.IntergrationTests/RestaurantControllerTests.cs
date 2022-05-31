1716c2ede34943d26e1530bb74ea1e68c530b539
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-31 10:39:25 +0200

    added using database in memory

--- RestaurantAPI.IntergrationTests/RestaurantControllerTests.cs
+++ RestaurantAPI.IntergrationTests/RestaurantControllerTests.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
 using FluentAssertions;
 using System.Net.Http;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Entities;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace RestaurantAPI.IntergrationTests
 {
@@ -17,7 +20,22 @@ namespace RestaurantAPI.IntergrationTests
 
         public RestaurantControllerTests(WebApplicationFactory<Startup> factory)
         {
-            _client = factory.CreateClient();
+            _client = factory
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(service =>
+                    {
+                        var dbContextOptions = service
+                            .FirstOrDefault(service =>
+                            service.ServiceType == typeof(DbContextOptions<RestaurantDbContext>));
+
+                        service.Remove(dbContextOptions);
+
+                        service.AddDbContext<RestaurantDbContext>(options =>
+                            options.UseInMemoryDatabase("RestaurantDbInMemory"));
+                    });
+                })
+                .CreateClient();
         }
 
         [Theory]
